8facc6f83917035a222958fbe53ada8a3d4404bb
RishabhMX <c-rishabh.singh@mxplayer.in>
2022-06-22 15:28:09 +0530

    Added Models

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'core',
 ]
 
@@ -127,3 +128,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_MODEL_USER = 'core.User'  #setting authorization to use User model
